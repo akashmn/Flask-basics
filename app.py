55f90300bc31844a5a19c0f34446aff2fb4b97fc
Akash M Nandan <akash003nandan@gmail.com>
2025-01-17 00:14:21 +0530

    rendering html templates at '/'

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return '<h1>Home page</h1>'
+    return render_template('index.html')
 
 @app.route('/about', methods=['POST', 'GET'])
 def about():
