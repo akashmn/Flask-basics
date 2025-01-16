e0696e613b8f102a85ecd27ba6c385c40dd67b18
Akash M Nandan <akash003nandan@gmail.com>
2025-01-16 22:00:38 +0530

    created routes /about and /contact

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hey, World!</h1>'
+    return '<h1>Home page</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
+
+@app.route('/contact')
+def contact():
+    return '<h1>Contact Page</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True) 
