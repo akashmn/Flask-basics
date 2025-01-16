a8d6154be51417d90b0a3871d5d0c0ecfba43aab
Akash M Nandan <akash003nandan@gmail.com>
2025-01-16 22:09:58 +0530

    passing variables to urls and to routes

--- app.py
+++ app.py
@@ -14,5 +14,17 @@ def about():
 def contact():
     return '<h1>Contact Page</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello, {name}</h1>'
+
+# @app.route('/user/<id1>/<id2>')
+# def addString(id1, id2):
+#     return f'<h1>{id1} + {id2} = {id1 + id2}</h1>' // This will concatenate the strings
+
+@app.route('/user/<int:id1>/<int:id2>')
+def add(id1, id2):
+    return f'<h1>{id1} + {id2} = {id1 + id2}</h1>'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True) 
